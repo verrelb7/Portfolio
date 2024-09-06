e15d6ac6ccf1e4cb2f2da467cbe9dd935078ea11
verrelb7 <verrelb7@gmail.com>
2024-09-06 14:26:29 +0700

    add contact

--- main.js
+++ main.js
@@ -13,3 +13,26 @@ function toggleDetail(e) {
 
     $(detail).slideToggle()
 } 
+
+function onFormSubmit (e) {
+    e.preventDefault()
+
+    const email = $("inp-email")
+    const subject = $("inp-subject")
+    const message = $("inp-message")
+
+    if(!$(email).val()) {
+        alert("Email is Required")
+    } else if (!$(subject).val()) {
+        alert ("Subject is Required")
+    } else if (!$(message).val()) {
+        alert ("Message is Required")
+    } else {
+        alert ("Form Submitted")
+        $(email).val("")
+        $(subject).val("")
+        $(message).val("")
+
+
+    }
+}
